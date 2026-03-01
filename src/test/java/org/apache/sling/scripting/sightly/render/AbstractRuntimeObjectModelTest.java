e64e5172684ae124374899576438ff735f5e6236
Bhavik Kothari <20669150+bkkothari2255@users.noreply.github.com>
2026-03-01 22:08:55 +0530

    SLING-13054: Migrate AbstractRuntimeObjectModelTest and ObjectModelTest to JUnit 5

--- src/test/java/org/apache/sling/scripting/sightly/render/AbstractRuntimeObjectModelTest.java
+++ src/test/java/org/apache/sling/scripting/sightly/render/AbstractRuntimeObjectModelTest.java
@@ -30,9 +30,12 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.sling.scripting.sightly.Record;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AbstractRuntimeObjectModelTest {
 
--- src/test/java/org/apache/sling/scripting/sightly/render/ObjectModelTest.java
+++ src/test/java/org/apache/sling/scripting/sightly/render/ObjectModelTest.java
@@ -37,14 +37,14 @@ import org.apache.sling.scripting.sightly.render.testobjects.TestEnum2;
 import org.apache.sling.scripting.sightly.render.testobjects.internal.AdultFactory;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ObjectModelTest {
 
